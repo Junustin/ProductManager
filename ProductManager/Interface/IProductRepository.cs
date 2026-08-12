7bcf305e1a0392bd2759619431c6ddceae4812c8
Junustin <kritapaswinichayakul@gmail.com>
2026-08-12 19:59:32 +0700

    IProductRepository and install Sqlite and dapper package

---
+++ ProductManager/Interface/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductManager.Features.ProductComponents.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Interface
+{
+	public interface IProductRepository
+	{
+		void InitilizeDatabase();
+		Product Add(Product product);
+		IEnumerable<Product> GetAll();
+	}
+}
